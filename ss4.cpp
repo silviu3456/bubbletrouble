02ea7a819405e17274d2a0056107213801fe19aa
silviu3456 <35564574+silviu3456@users.noreply.github.com>
2018-03-22 12:15:44 +0200

    Add files via upload

---
+++ ss4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<limits.h>
+
+using namespace std;
+int V[100];
+int n;
+int main()
+{
+cout<<"n=";cin>>n;
+for(int i=1;i<=n;i++)
+    {   cout<<"V["<<i<<"]=";
+        cin>>V[i];
+    }
+for(int i=1;i<=n;i++)
+    {
+        int minim=INT_MAX;
+        int locul=1;
+        for(int j=i;j>=n;j--)
+        {
+           if (V[j]<minim) {
+                                   minim=V[j];
+                                   locul=j;
+                                  }
+        }
+        swap(V[i],V[locul]);
+    }
+cout<<endl<<"Sortate "<<endl;
+for(int i=1;i<=n;i++)
+    cout<<V[i]<<" ";
+}
